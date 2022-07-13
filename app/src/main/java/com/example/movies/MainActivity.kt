c882c54500affd3db80194e21f267b1c1bfa53ff
Aleksandr Sergeev <alex.dev.sergeev@gmail.com>
2022-07-13 10:06:27 +0300

    adding recyclerView adapter and his logic

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.movies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.domain.models.Movie
 import com.example.movies.presentation.MainViewModel
+import com.example.movies.presentation.recycler_view.MoviesAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var liveData: LiveData<List<Movie>>
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progress_bar)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         liveData = viewModel.getMovieLiveData()
@@ -24,7 +33,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun bindLiveData(){
         liveData.observe(this){
-            Log.e("RESULT_OBSERVE", it.toString())
+            progressBar.visibility = View.INVISIBLE
+            initRecyclerView(it)
+
         }
+    }
+    
+    private fun initRecyclerView(list: List<Movie>){
+        val rv = findViewById<RecyclerView>(R.id.movies_rw)
+        val adapter = MoviesAdapter()
+        rv.adapter = adapter
+        rv.layoutManager = GridLayoutManager(this, 2)
+        adapter.initListMovie(list)
+
     }
 }
